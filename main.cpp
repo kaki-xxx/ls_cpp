41cdcd3d99ae225d4cbc8237cf4ba6b63cbe9081
kaki <suka6411@gmail.com>
2021-06-13 18:42:36 +0900

    feat: 端末の幅と高さの取得を実装

--- main.cpp
+++ main.cpp
@@ -4,15 +4,26 @@
 #include <vector>
 #include <algorithm>
 #include <filesystem>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 namespace fs = std::filesystem;
 
 int main() {
     auto iter = fs::directory_iterator(".");
-    std::vector<fs::directory_entry> files(begin(iter), end(iter));
+    std::vector files(begin(iter), end(iter));
     std::sort(std::begin(files), std::end(files));
     for (const auto& x : files) {
         std::cout << x.path().filename().generic_u8string() << std::endl;
     }
+
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) {
+		perror("ioctl");
+	}
+
+	std::cout << "width: " <<  ws.ws_col << std::endl;
+	std::cout << "height: " <<  ws.ws_row << std::endl;
     return 0;
 }
