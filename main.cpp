e4c7aee4b468a053e15e678fcb0a92354699a043
kaki <suka6411@gmail.com>
2021-06-13 17:53:28 +0900

    feat: 辞書順にソートされるようにした

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#include <bits/types/FILE.h>
 #include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 #include <filesystem>
 
 namespace fs = std::filesystem;
 
 int main() {
-    for (const auto& x : fs::directory_iterator(".")) {
+    auto iter = fs::directory_iterator(".");
+    std::vector<fs::directory_entry> files(begin(iter), end(iter));
+    std::sort(std::begin(files), std::end(files));
+    for (const auto& x : files) {
         std::cout << x.path().filename().generic_u8string() << std::endl;
     }
     return 0;
