1dcf0af829cc99d185824b418eda8192b6679c15
kaki <suka6411@gmail.com>
2021-06-13 17:46:52 +0900

    feat: 単純なlsっぽいものを作成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
-    std::cout << "Hello, world" << std::endl;
+    for (const auto& x : fs::directory_iterator(".")) {
+        std::cout << x.path().filename().generic_u8string() << std::endl;
+    }
     return 0;
 }
