47bda435f91e4869893c2d501e1e9fc4a2c5b955
kaki <suka6411@gmail.com>
2021-06-26 15:30:06 +0900

    feat: lsっぽいカラム表示を実装

--- ls.cc
+++ ls.cc
@@ -1,9 +1,12 @@
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include <filesystem>
 #include <algorithm>
 #include <sys/ioctl.h>
+#include <vector>
 #include "ls.h"
 
 namespace fs = std::filesystem;
@@ -25,9 +28,24 @@ Ls::Ls() : terminal_size(GetTerminalSize()) {}
 
 void Ls::run() {
     auto iter = fs::directory_iterator(".");
-    std::vector files(begin(iter), end(iter));
-    std::sort(std::begin(files), std::end(files));
-    for (const auto& x : files) {
-        std::cout << x.path().filename().generic_u8string() << std::endl;
+    std::vector filepaths(begin(iter), end(iter));
+    std::sort(std::begin(filepaths), std::end(filepaths));
+    size_t display_len = 0;
+    std::vector<std::string> files;
+    files.reserve(filepaths.size());
+    for (const auto& file : filepaths) {
+        std::string filename = file.path().filename().generic_u8string();
+        files.push_back(filename);
+        display_len = std::max(display_len, filename.length() + 2);
     }
+    size_t number_per_onerow = terminal_size.col / display_len;
+    size_t number_of_rows = (filepaths.size() + number_per_onerow-1) / number_per_onerow;
+    std::ios::fmtflags prev_flags = std::cout.setf(std::ios::left, std::ios::adjustfield);
+    for (size_t row = 0; row < number_of_rows; row++) {
+        for (size_t col = row; col < filepaths.size(); col += number_of_rows) {
+            std::cout << std::setw(display_len) << files[col];
+        }
+        std::cout << std::endl;
+    }
+    std::cout.flags(prev_flags);
 }
