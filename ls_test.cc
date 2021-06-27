18fcad978761069a7c36956ae437a25eccc27730
kaki <suka6411@gmail.com>
2021-06-27 21:50:07 +0900

    feat: 隠しフォルダの表示有無のテストを書いた

--- ls_test.cc
+++ ls_test.cc
@@ -20,4 +20,19 @@ TEST(ListSortedEntriesIn, IsSorted) {
     EXPECT_EQ(ret[0], fs::path("./aaa"));
     EXPECT_EQ(ret[1], fs::path("./aba"));
     EXPECT_EQ(ret[2], fs::path("./abb"));
+    chdir("..");
+}
+
+TEST(ListSortedEntriesIn, IgnoreHiddenFile) {
+    char filename[] = "XXXXXX";
+    const char *temp_dir = mkdtemp(filename);
+    chdir(temp_dir);
+    std::ofstream(".aaa");
+    std::ofstream("aaa");
+    std::ofstream("bbb");
+    auto ret2 = ListSortedEntriesIn(".", false);
+    EXPECT_EQ(ret2.size(), 3);
+    auto ret1 = ListSortedEntriesIn(".", true);
+    EXPECT_EQ(ret1.size(), 2);
+    chdir("..");
 }
