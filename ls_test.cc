c12086a2b7efd6ffef313e0ec3e9fd8851a11a9f
kaki <suka6411@gmail.com>
2021-06-27 22:52:11 +0900

    feat: テストをいろいろ追加

--- ls_test.cc
+++ ls_test.cc
@@ -5,34 +5,38 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+#include <vector>
 #include "ls.cc"
 
 namespace fs = std::filesystem;
 
-TEST(ListSortedEntriesIn, IsSorted) {
+std::string MkTempDirAndCreateFiles(std::vector<std::string> files) {
     char filename[] = "XXXXXX";
     const char *temp_dir = mkdtemp(filename);
-    chdir(temp_dir);
-    std::ofstream("aaa");
-    std::ofstream("aba");
-    std::ofstream("abb");
-    auto ret = ListSortedEntriesIn(".");
-    EXPECT_EQ(ret[0], fs::path("./aaa"));
-    EXPECT_EQ(ret[1], fs::path("./aba"));
-    EXPECT_EQ(ret[2], fs::path("./abb"));
-    chdir("..");
+    for (auto file : files) {
+        std::ofstream(fs::path(temp_dir) / file);
+    }
+    return std::string(filename);
+}
+
+TEST(ListSortedEntriesIn, IsSorted) {
+    auto temp_dir = MkTempDirAndCreateFiles({"aaa", "aba", "abb"});
+    auto ret = ListSortedEntriesIn(temp_dir);
+    EXPECT_EQ(ret[0], fs::path(temp_dir) / "aaa");
+    EXPECT_EQ(ret[1], fs::path(temp_dir) / "aba");
+    EXPECT_EQ(ret[2], fs::path(temp_dir) / "abb");
 }
 
 TEST(ListSortedEntriesIn, IgnoreHiddenFile) {
-    char filename[] = "XXXXXX";
-    const char *temp_dir = mkdtemp(filename);
-    chdir(temp_dir);
-    std::ofstream(".aaa");
-    std::ofstream("aaa");
-    std::ofstream("bbb");
-    auto ret2 = ListSortedEntriesIn(".", false);
-    EXPECT_EQ(ret2.size(), 3);
-    auto ret1 = ListSortedEntriesIn(".", true);
-    EXPECT_EQ(ret1.size(), 2);
-    chdir("..");
+    auto temp_dir = MkTempDirAndCreateFiles({".aaa", "aba", "abb"});
+    auto ret1 = ListSortedEntriesIn(temp_dir, false);
+    EXPECT_EQ(ret1.size(), 3);
+    auto ret2 = ListSortedEntriesIn(temp_dir, true);
+    EXPECT_EQ(ret2.size(), 2);
+}
+
+TEST(GetFileInfo, FiletypeAndPermisson) {
+    auto temp_dir = MkTempDirAndCreateFiles({"test"});
+    auto file_info1 = GetFileInfo(fs::path(temp_dir) / "test");
+    EXPECT_EQ(file_info1.filetype_permisson, "-rw-r--r--");
 }
