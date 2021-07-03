e6cedeec5563e7b88617cbdb4c75ebe813817c98
kaki <suka6411@gmail.com>
2021-07-03 16:52:03 +0900

    feat: バージョン情報の表示(--version)オプションを追加

--- main.cc
+++ main.cc
@@ -1,7 +1,15 @@
 #include "ls.h"
 #include "cxxopts.hpp"
+#include "config.h"
+#include <cstdio>
 #include <cstdlib>
 #include <system_error>
+
+std::string VersionInfo() {
+    char buf[16];
+    std::snprintf(buf, sizeof(buf) - 1, "ls (%d.%d)", LS_VERSION_MAJOR, LS_VERSION_MINOR);
+    return std::string(buf);
+}
 
 int main(int argc, char *argv[]) {
     setlocale(LC_CTYPE, "");
@@ -10,12 +18,17 @@ int main(int argc, char *argv[]) {
         ("l", "use a long listing format")
         ("a,all", "do not ignore entries starting with .")
         ("help", "display this help and exit")
+        ("version", "show version information")
     ;
     try {
         auto result = options.parse(argc, argv);
         options.custom_help("[OPTION]... [FILE]...");
         if (result.count("help")) {
             std::cout << options.help() << std::endl;
+            std::exit(EXIT_SUCCESS);
+        }
+        if (result.count("version")) {
+            std::cout << VersionInfo() << std::endl;
             std::exit(EXIT_SUCCESS);
         }
         auto args = result.unmatched();
